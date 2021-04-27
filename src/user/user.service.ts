c8a8872a4b9be4dfcb8fbf5ab9dd136a8122ca7e
Abel Souza <abelsouzacosta@gmail.com>
2021-04-27 13:33:22 -0300

    feat(users): adicionando e listando usuários

---
+++ src/user/user.service.ts
@@ -0,0 +1,42 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private repository: Repository<User>,
+  ) {}
+
+  public async findAll(): Promise<User[]> {
+    const users = this.repository.find();
+
+    return users;
+  }
+
+  public async showUser(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+
+  public async create(data: CreateUserInput): Promise<User> {
+    const user = this.repository.create(data);
+
+    const userSaved = await this.repository.save(user);
+
+    if (!userSaved)
+      throw new InternalServerErrorException('User nao pode ser salvo');
+
+    return userSaved;
+  }
+}
